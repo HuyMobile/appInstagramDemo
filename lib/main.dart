bd8e6af7f02402c0445a1bb32670df966f4e6653
huy <huyvqmobile@gmail.com>
2022-12-09 14:34:30 +0700

    setting up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:appinstagram/responsive/responsive_layout_screen.dart';
 import 'package:appinstagram/responsive/web_screen_layout.dart';
 import 'package:appinstagram/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
